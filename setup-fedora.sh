4fdaa8e39347fd4112ab4dd416d7ff98d097d8a4
Lars Hamre <chemecse@gmail.com>
2016-04-04 11:01:37 -0400

    setup-fedora: fixed NAME setting in gitconfig

--- setup-fedora.sh
+++ setup-fedora.sh
@@ -11,12 +11,13 @@ sudo dnf install -y gcc-c++
 sudo dnf install -y git-email
 
 git config --global user.email $EMAIL
-git config --global user.name $NAME
+git config --global user.name "$NAME"
 git config --global core.editor vim
 git config --global sendemail.smtpserver smtp.gmail.com
 git config --global sendemail.smtpserverport 587
 git config --global sendemail.smtpencryption tls
 git config --global sendemail.smtpuser $EMAIL
+exit
 
 ### mesa development setup
 
